11df9bd87e1514fc8a5ce8577ecc92f67003d6b8
richardrixham <richardrixham@hotmail.com>
2015-12-14 16:09:07 +0000

    Initial Commit

---
+++ WebApplication1/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class TestController : Controller
+    {
+        public string GetString()
+        {
+            return "Hello World is old now. It&rsquo;s time for wassup Bro;)";
+        }
+
+        public ActionResult GetView()
+        {
+            return View("MyView");
+        }
+    }
+}
